70893123ba5d63750606ca418be00d7956feb006
matthias <yasuakii@protonmail.com>
2022-11-16 16:14:14 +0100

    react version

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Page from './components/Page';
+
+function App() {
+  return (
+  <div className="page">
+    <Page />
+  </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+/* GPL-3.0 License > https://github.com/Luna-devv/Modern-Website/blob/main/LICENSE*/
+/* Please don't remove this! */
+export default function Footer() {
+    return(
+        <footer id='bottom-footer'>
+            <div className='center-footer'>©️ Template made by <a className='saho' href='https://lunish.nl/luna'>Luna</a>
+
+            </div>
+        </footer>
+    )
+}
+
+/* Please don't remove this! */
